50c54919c177cda9e5293762ccf60082e63dd89a
RekiRaki <tsunamichan@gmail.com>
2015-04-22 14:38:20 -0700

    Edited ForgotPassword layout and activity

--- Aqua Scout/src/main/java/com/example/aquascout/ForgotPasswordActivity.java
+++ Aqua Scout/src/main/java/com/example/aquascout/ForgotPasswordActivity.java
@@ -1,9 +1,13 @@
 package com.example.aquascout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Tsunami on 4/14/2015.
@@ -14,6 +18,16 @@ public class ForgotPasswordActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        Button submit = (Button) findViewById(R.id.submitButton);
+        submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ForgotPasswordActivity.this, SignInActivity.class);
+                Toast.makeText(ForgotPasswordActivity.this, "Please check your email to reset your password", Toast.LENGTH_LONG).show();
+                startActivity(intent);
+            }
+        });
 
 
     }
